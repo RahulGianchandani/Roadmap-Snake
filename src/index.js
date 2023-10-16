a514e448debef9ad0f3dc38ee8db665fe979b217
admrahul <107991710+admrahul@users.noreply.github.com>
2023-10-16 18:13:39 +0500

    asda

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router b>
+    <Router >
       <Routes>
-        <Route path="/" element={<Timeline />} />
+        <Route path="/Roadmap-Snake" element={<Timeline />} />
       </Routes>
     </Router>
   </React.StrictMode>,
